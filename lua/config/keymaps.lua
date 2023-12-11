58c6692a6c6795778e0185a1f067353a5c883e07
Muneeb Usmani <muneebusmani8355@gmail.com>
2023-12-11 18:54:10 +0500

    Finalized NVim Setup

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local set=vim.keymap.set
+-- Copy Line Down : Ctrl+D
+set('n',"<C-d>","VYp")
+set('i',"<C-d>","<C-c>VYpi")
+
+-- Delete Line : Ctrl+F
+set('n',"<C-f>","dd")
+set('i',"<C-f>","<C-c>ddi")
