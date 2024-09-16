820bd5b0c8ffa1b8324615616e3c223f88d276fd
muneebusmani <muneebusmani8355@gmail.com>
2024-09-17 04:11:48 +0500

    -m

--- lua/plugins/edgy.lua
+++ lua/plugins/edgy.lua
@@ -1,11 +1,11 @@
 return {
-  "folke/edgy.nvim",
-  event = "VeryLazy",
-  opts = {
-    animate = {
-      enabled = true,
-      fps = 60, -- frames per second
-      cps = 60, -- cells per second
-    },
-  },
+  -- "folke/edgy.nvim",
+  -- event = "VeryLazy",
+  -- opts = {
+  --   animate = {
+  --     enabled = true,
+  --     fps = 60, -- frames per second
+  --     cps = 60, -- cells per second
+  --   },
+  -- },
 }
