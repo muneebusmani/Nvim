96391910d0f6c83bef18b37fdc1ea39ddf52e662
Muneeb Usmani <muneebusmani8355@gmail.com>
2024-12-13 00:00:41 +0500

    optimized for laravel

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -1,11 +1,11 @@
 return {
-  "akinsho/flutter-tools.nvim",
-  -- lazy = true,
-  ft = "dart",
-  -- event = "VeryLazy",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "stevearc/dressing.nvim", -- optional for vim.ui.select
-  },
-  opts = {},
+  -- "akinsho/flutter-tools.nvim",
+  -- -- lazy = true,
+  -- ft = "dart",
+  -- -- event = "VeryLazy",
+  -- dependencies = {
+  --   "nvim-lua/plenary.nvim",
+  --   "stevearc/dressing.nvim", -- optional for vim.ui.select
+  -- },
+  -- opts = {},
 }
